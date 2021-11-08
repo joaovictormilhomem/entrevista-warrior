1702ed2c03b208e3c486b9037f964909b6eac353
João Victor Milomem <joaovictormilho@gmail.com>
2021-11-07 22:28:33 -0300

    connection to api, get questions, decode questions and show questions implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import api from './services/api'
 
 function App() {
+
+  const [questions, setQuestions] = useState();
+  
+  useEffect(() => {
+    api
+      .get("/api.php?amount=5")
+      .then((response) => setQuestions(response.data.results))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      Hello World!
+      {questions ? questions.map((questionData, index) => <p key={index} dangerouslySetInnerHTML={{__html:questionData.question}}/>) : <p>Loading...</p>}
     </div>
   );
 }
